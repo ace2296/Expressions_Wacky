2535cd25d502f2859c1d954ad5d59d246b3dd94f
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 20:28:40 -0800

    fixed calculation error

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Guerra, Andre 12/13/14 Expressions - Wacky
 
-var health = prompt("What is your overall fitness level 1–10?");
+var health = prompt("What is your overall fitness level 1 through 10?");
 var age = prompt("What is your age?");
 var waist = prompt("What is your waist size?")
 var cavities = prompt("How many cavities do you have?");
@@ -8,7 +8,7 @@ var totalTeeth = prompt("How many bottom teeth do you have?");
 
 totalTeeth += 16
 
-var jellyBeans = (health + age) * waist / cavities + totalTeeth;
+var jellyBeans = (health * age) + (waist * totalTeeth) - cavities;
 
 alert("After calculating your inputs, we concluded that you can eat a total of " + jellyBeans + " jelly beans in one sitting. That's impressive!")
 
