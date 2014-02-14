9e555d9251a5a460fa9f4ef1710d80ba21aebf15
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 20:07:30 -0800

    age and shirt var

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 //Guerra, Andre 12/13/14 Expressions - Wacky
 
 var health = prompt("What is your overall fitness level 1–10?");
+var age = prompt("What is your age?");
+var shirt = prompt("What size of shirt do you wear?");
+
