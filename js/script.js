c5941d39e9835f0930c820ad9d267de6de4b843d
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 20:11:51 -0800

    cavities var

--- js/script.js
+++ js/script.js
@@ -4,4 +4,6 @@ var health = prompt("What is your overall fitness level 1–10?");
 var age = prompt("What is your age?");
 var shirt = prompt("What size of shirt do you wear?");
 var pants = prompt("What is your waist size?")
+var cavities = prompt("How many cavities do you have?");
+
 
