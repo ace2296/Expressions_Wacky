5148ae7d454ef8e48978c10156e2a37e8223b7f8
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 20:08:11 -0800

    pants var

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@
 var health = prompt("What is your overall fitness level 1–10?");
 var age = prompt("What is your age?");
 var shirt = prompt("What size of shirt do you wear?");
+var pants = prompt("What is your waist size?")
 
