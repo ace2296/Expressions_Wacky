4b83b78a6f6f4309fbb7fe938b4b09a4ed834545
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 20:30:25 -0800

    note

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var health = prompt("What is your overall fitness level 1 through 10?"); //Sets 
 var age = prompt("What is your age?"); //This is multiplied by the health
 var waist = prompt("What is your waist size?") //This is multiplied by the total teeth
 var cavities = prompt("How many cavities do you have?"); //This will take away fromy our total number
-var totalTeeth = prompt("How many bottom teeth do you have?");
+var totalTeeth = prompt("How many bottom teeth do you have?"); //This will double up to come out with the total teeth you have
 
 totalTeeth += 16
 
