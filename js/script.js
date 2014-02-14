808970b1881a4fd88e804b6adaf3da910af0f902
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 20:34:00 -0800

    example added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 var health = prompt("What is your overall fitness level 1 through 10?"); //Sets the based number for jelly bean equation
 var age = prompt("What is your age?"); //This is multiplied by the health
-var waist = prompt("What is your waist size?") //This is multiplied by the total teeth
+var waist = prompt("What is your waist size? i.e. 28, 29, 30, 6, 8") //This is multiplied by the total teeth
 var cavities = prompt("How many cavities do you have?"); //This will take away fromy our total number
 var totalTeeth = prompt("How many bottom teeth do you have?"); //This will double up to come out with the total teeth you have
 
@@ -17,5 +17,3 @@ alert("After calculating your inputs, we concluded that you can eat a total of "
 
 //This will be stored in console to read at a later time
 console.log("After calculating your inputs, we concluded that you can eat a total of " + jellyBeans + " jelly beans in one sitting. That's impressive!");
-
-
